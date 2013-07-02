b229528488fc0345f0341bb676af0d8f36f5ddea
Toke Høiland-Jørgensen <toke@toke.dk>
2013-07-02 12:57:26 +0200

    Statistics, the way it should be...

--- src/sender.c
+++ src/sender.c
@@ -25,27 +25,28 @@ static unsigned short gen_port()
 	return PORT_START + rand() % PORT_RANGE;
 }
 
-static unsigned int calc_delay(int family, int rate)
-{
-	int size = OVERHEAD(family) + PAYLOAD;
-	return 1000000 / (rate/size);
-}
-
-static unsigned int exp_distrib(unsigned int delay)
+static unsigned int exp_distrib(unsigned int pps)
 {
 	// Draw a new delay from the exponential distribution with mean delay
-	// set to the parameter. Calculated by -ln(r)/delay where r is a random
-	// number between 0 and 1
-	double r = (double)rand() / (double) RAND_MAX;
-	if(r == 0.0) return 0; // FIXME not correct; what do to?
-	int d = (unsigned int) (-log(r)/((double)delay/2000000.0));
+	// set to achieve pps packets/sec. Calculated by -ln(r)/pps where r is a
+	// random number between 0 and 1
+	//
+	// Ref: https://en.wikipedia.org/wiki/Exponential_distribution#Generating_exponential_variates
+	double r;
+	do {
+		r = (double)rand() / (double) RAND_MAX;
+	} while(r == 0.0);
+	int d = (unsigned int) 1000000 * (-log(r)/((double)pps));
 	return d;
 }
 
-static void schedule_next(unsigned int delay, char poisson, struct timeval *now, struct timeval *next)
+static void schedule_next(unsigned int pps, char poisson, struct timeval *now, struct timeval *next)
 {
+	int delay;
 	if(poisson)
-		delay = exp_distrib(delay);
+		delay = exp_distrib(pps);
+	else
+		delay = 1000000/pps;
 	next->tv_sec = now->tv_sec;
 	next->tv_usec = now->tv_usec + delay;
 	if(next->tv_usec > 1000000) {
@@ -58,15 +59,14 @@ void send_loop(struct options *opt)
 {
 	struct timeval now, next, stop;
 	char msg[PAYLOAD] = {0};
-	unsigned int delay = calc_delay(opt->dest.ai_family, opt->rate);
+	unsigned int pps = opt->rate / (OVERHEAD(opt->dest.ai_family) + PAYLOAD);
 
 	gettimeofday(&now, NULL);
 	stop.tv_sec = now.tv_sec + opt->run_length;
 	stop.tv_usec = now.tv_usec;
 	srand(now.tv_sec ^ now.tv_usec);
-	printf("Size: %d\n", OVERHEAD(opt->dest.ai_family) + PAYLOAD);
 	do {
-		schedule_next(delay, opt->poisson, &now, &next);
+		schedule_next(pps, opt->poisson, &now, &next);
 		while(now.tv_sec < next.tv_sec || now.tv_usec < next.tv_usec) {
 			if(next.tv_usec - now.tv_usec > USLEEP_THRESHOLD)
 				usleep(USLEEP_THRESHOLD);
